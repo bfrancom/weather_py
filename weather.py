6ae9658576252b3f2b139b4603851862ea733afa
Ben Francom <bfrancom@gmail.com>
2018-05-25 16:02:46 -0600

    Create weather.py

---
+++ weather.py
@@ -0,0 +1,13 @@
+import requests
+
+#DarkSky api requires an API key. Please get your own. :)
+req = requests.get('https://api.darksky.net/forecast/1ca512d2c3996c9c4b83c2688d094162/40.7608,-111.8910')
+result = req.json()
+
+summary = result['currently']['summary']
+temperature = result['currently']['temperature']
+print(summary,"  ",temperature,"°F",sep='')
+#if summary == 'Clear':
+#	print(summary,"  ",temperature,"°F",sep='')
+#else:
+#	print("whoDat?")
